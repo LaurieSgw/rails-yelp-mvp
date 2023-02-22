4b36de418aca6f1374758ca8d232c7a744a44d92
Laurie Segalowitch <lausegalo@gmail.com>
2023-02-22 15:59:24 +0100

    Yelp Rails - Views done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,28 @@
 class ReviewsController < ApplicationController
-  def index
+  before_action :set_restaurant, only: %i[new create]
 
+  def index
+    @reviews = Review.all
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   CATEGORIES = %w[chinese italian japanese french belgian].freeze
   has_many :reviews, dependent: :destroy
 
-  validates :name, :address, :phone_number, presence: true
+  validates :name, :address, :phone_number, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES, message: '{value} is not a valid category' }
 end
